7b1fbb1f928fa95eb67cb7feec1135ba636b71ad
Артём <gartdy@gmail.com>
2022-10-21 20:31:48 +0300

    Add fields validation.

--- src/main/java/org/artdy/controllers/PeopleController.java
+++ src/main/java/org/artdy/controllers/PeopleController.java
@@ -4,7 +4,10 @@ import org.artdy.dao.PersonDao;
 import org.artdy.models.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
@@ -34,7 +37,10 @@ public class PeopleController {
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person) {
+    public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "people/new";
+        }
         personDao.save(person);
         return "redirect:/people";
     }
@@ -46,7 +52,10 @@ public class PeopleController {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult, @PathVariable("id") int id) {
+        if (bindingResult.hasErrors()) {
+            return "people/edit";
+        }
         personDao.update(id, person);
         return "redirect:/people";
     }
--- src/main/java/org/artdy/models/Person.java
+++ src/main/java/org/artdy/models/Person.java
@@ -1,11 +1,19 @@
 package org.artdy.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class Person {
     private int id;
-    //@NotEmpty(message = "Name shouldn't be empty!")
-    //@Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters!")
+    @NotEmpty(message = "Name shouldn't be empty!")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters!")
     private String name;
+    @Min(value = 1, message = "Age should be greater than 0!")
     private int age;
+    @NotEmpty(message = "Email shouldn't be empty!")
+    @Email(message = "Email should be valid!")
     private String email;
 
     public Person(int id, String name, int age, String email) {
