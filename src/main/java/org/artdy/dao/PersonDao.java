ceaa7a96cfdb72d13f620b4e58fa56ad41f288d6
Артём <gartdy@gmail.com>
2022-10-30 21:15:31 +0300

    Add filed address for Person and  address validation.

--- src/main/java/org/artdy/dao/PersonDao.java
+++ src/main/java/org/artdy/dao/PersonDao.java
@@ -33,12 +33,12 @@ public class PersonDao {
     }
 
     public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO Person(name, age, email) VALUES(?, ?, ?)", person.getName(), person.getAge(),
-                person.getEmail());
+        jdbcTemplate.update("INSERT INTO Person(name, age, email, address) VALUES(?, ?, ?, ?)", person.getName(), person.getAge(),
+                person.getEmail(), person.getAddress());
     }
 
     public void update(int id, Person updatedPerson) {
-        jdbcTemplate.update("UPDATE Person SET name=?, age=?, email=? WHERE id=?", updatedPerson.getName(),
+        jdbcTemplate.update("UPDATE Person SET name=?, age=?, email=?, address=? WHERE id=?", updatedPerson.getName(),
                 updatedPerson.getAge(), updatedPerson.getEmail(), id);
     }
 
--- src/main/java/org/artdy/models/Person.java
+++ src/main/java/org/artdy/models/Person.java
@@ -1,9 +1,6 @@
 package org.artdy.models;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 public class Person {
     private int id;
@@ -15,12 +12,15 @@ public class Person {
     @NotEmpty(message = "Email shouldn't be empty!")
     @Email(message = "Email should be valid!")
     private String email;
+    @Pattern(regexp = "[A-Z]\\w+, [A-Z]\\w+, \\d{6}", message = "Address should be in format: Country, City, Postcode(6 digits)!")
+    private String address;
 
-    public Person(int id, String name, int age, String email) {
+    public Person(int id, String name, int age, String email, String address) {
         this.id = id;
         this.name = name;
         this.age = age;
         this.email = email;
+        this.address = address;
     }
 
     public Person() {
@@ -57,5 +57,13 @@ public class Person {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
